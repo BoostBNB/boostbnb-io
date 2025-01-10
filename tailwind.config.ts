0f74c59b19382aff4c6f95c2037fb6d96a5474c7
April Hall <ari@arithefirst.com>
2025-01-10 17:48:02 -0500

    build: Setup DaisyUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui'
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -7,5 +8,18 @@ export default {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          "primary": "#d496c1",
+          "secondary": "#fbd0ed",
+          "accent": "#6981ce",
+          "neutral": "#262626",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
 } satisfies Config;
