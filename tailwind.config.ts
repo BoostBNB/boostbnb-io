ad4754e9251b96c6d55ae5e1389f9028ebb15d57
April Hall <ari@arithefirst.com>
2025-01-10 20:06:55 -0500

    feat: Set Tailwind font to 'Funnel Sans'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,43 @@
 import type { Config } from 'tailwindcss';
-import daisyui from 'daisyui'
+import daisyui from 'daisyui';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+  // Tailwind Config
+  theme: {
+    extend: {
+      fontSize: {
+        sm: '0.750rem',
+        base: '1rem',
+        xl: '1.333rem',
+        '2xl': '1.777rem',
+        '3xl': '2.369rem',
+        '4xl': '3.158rem',
+        '5xl': '4.210rem',
+      },
+      fontFamily: {
+        heading: 'Funnel Sans',
+        body: 'Funnel Sans',
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
+      },
+    },
+  },
 
-	plugins: [daisyui],
+  // DaisyUI Config
+  plugins: [daisyui],
   daisyui: {
     themes: [
       {
         light: {
-          "primary": "#d496c1",
-          "secondary": "#fbd0ed",
-          "accent": "#6981ce",
-          "neutral": "#262626",
-          "base-100": "#ffffff",
+          primary: '#d496c1',
+          secondary: '#fbd0ed',
+          accent: '#6981ce',
+          neutral: '#262626',
+          'base-100': '#ffffff',
         },
       },
     ],
