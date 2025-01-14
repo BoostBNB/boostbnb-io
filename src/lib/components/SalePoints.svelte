01cdad676427775fb682586d732d8946c27db3d8
April Hall <ari@arithefirst.com>
2025-01-13 22:17:54 -0500

    fix: Adjust components for mobile support

--- src/lib/components/SalePoints.svelte
+++ src/lib/components/SalePoints.svelte
@@ -1,26 +1,26 @@
 <div class="my-16 text-center">
-  <h1 class="text-3xl">
+  <h1 class="text-2xl md:text-3xl">
     Boost your
     <span class="font-bold text-accent">Bookings</span>
     and increase
     <span class="font-bold text-accent">Revenue</span>
   </h1>
-  <div class="my-4 flex w-full justify-center gap-4">
-    <div class="card w-96 bg-base-200 shadow-xl">
+  <div class="my-4 flex w-full flex-col justify-center gap-4 md:flex-row">
+    <div class="card mx-auto w-96 bg-base-200 shadow-xl md:mx-0">
       <div class="card-body text-center">
         <img class="h-16" src="/icons/eye_line.svg" alt="mingcute:eye_line" />
         <h2 class="text-xl font-bold">Boost Visibility</h2>
         <p>Increase your visibility and reach a wider audience with our AI based Optimization Service.</p>
       </div>
     </div>
-    <div class="card w-96 bg-base-200 shadow-xl">
+    <div class="card mx-auto w-96 bg-base-200 shadow-xl md:mx-0">
       <div class="card-body text-center">
         <img class="h-16" src="/icons/book_6_line.svg" alt="mingcute:book_6_line" />
         <h2 class="text-xl font-bold">Turn Interest into Bookings</h2>
         <p>Turn potential guests into confirmed bookings with our smart and targeted AI algorithm based techniques.</p>
       </div>
     </div>
-    <div class="card w-96 bg-base-200 shadow-xl">
+    <div class="card mx-auto w-96 bg-base-200 shadow-xl md:mx-0">
       <div class="card-body text-center">
         <img class="h-16" src="/icons/chart_line_line.svg" alt="mingcute:chart_line_line" />
         <h2 class="text-xl font-bold">Stay ahead of market trends</h2>
