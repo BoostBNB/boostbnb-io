2a74919b80ba2db9c7da077f6aa79e312f5e28a6
April Hall <ari@arithefirst.com>
2025-01-17 14:21:52 -0500

    fix: Make company name and logo clickable

--- src/lib/layouts/Footer.svelte
+++ src/lib/layouts/Footer.svelte
@@ -1,9 +1,9 @@
 <footer class="text-base-content">
   <aside class="flex w-full flex-wrap items-center justify-between gap-2 bg-base-300 px-8 py-4 text-sm">
-    <p class="flex items-center gap-2 text-xl">
-      <img alt="Logo" src="/favicon.png" class="size-8" />
-      <span>BoostBNB</span>
-    </p>
+    <a class="btn btn-ghost text-lg" href="/">
+      <img alt="Logo" src="/favicon.png" class="w-8" />
+      BoostBNB
+    </a>
     <p>Copyright © 2023-{new Date().getFullYear()} - All rights reserved</p>
   </aside>
 </footer>
