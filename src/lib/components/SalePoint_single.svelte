93b2a3c1133efad5c0fb5f6db8760dbc764c1524
April Hall <ari@arithefirst.com>
2025-01-16 19:39:48 -0500

    style: Make salepoints component

---
+++ src/lib/components/SalePoint_single.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  interface props {
+    title: string;
+    desc: string;
+    src: string;
+    alt: string;
+  }
+
+  const { title, desc, src, alt }: props = $props();
+</script>
+
+<div class="card mx-auto w-11/12 bg-base-200 shadow-xl md:mx-0 md:w-auto">
+  <div class="card-body text-center">
+    <img class="h-16" {src} {alt} />
+    <h2 class="text-xl font-bold">{title}</h2>
+    <p>{desc}</p>
+  </div>
+</div>
--- src/lib/components/SalePoints.svelte
+++ src/lib/components/SalePoints.svelte
@@ -1,3 +1,7 @@
+<script>
+  import SalePointSingle from './SalePoint_single.svelte';
+</script>
+
 <div class="my-16 text-center">
   <h1 class="text-2xl md:text-3xl">
     Boost your
@@ -6,26 +10,23 @@
     <span class="font-bold text-accent">Revenue</span>
   </h1>
   <div class="flex w-full flex-col justify-center gap-4 p-4 md:flex-row lg:mx-auto lg:w-9/12">
-    <div class="card mx-auto w-11/12 bg-base-200 shadow-xl md:mx-0 md:w-auto">
-      <div class="card-body text-center">
-        <img class="h-16" src="/icons/eye_line.svg" alt="mingcute:eye_line" />
-        <h2 class="text-xl font-bold">Boost Visibility</h2>
-        <p>Increase your visibility and reach a wider audience with our AI based Optimization Service.</p>
-      </div>
-    </div>
-    <div class="card mx-auto w-11/12 bg-base-200 shadow-xl md:mx-0 md:w-auto">
-      <div class="card-body text-center">
-        <img class="h-16" src="/icons/book_6_line.svg" alt="mingcute:book_6_line" />
-        <h2 class="text-xl font-bold">Turn Interest into Bookings</h2>
-        <p>Turn potential guests into confirmed bookings with our smart and targeted AI algorithm based techniques.</p>
-      </div>
-    </div>
-    <div class="card mx-auto w-11/12 bg-base-200 shadow-xl md:mx-0 md:w-auto">
-      <div class="card-body text-center">
-        <img class="h-16" src="/icons/chart_line_line.svg" alt="mingcute:chart_line_line" />
-        <h2 class="text-xl font-bold">Stay ahead of market trends</h2>
-        <p>Stay ahead of market trends and ensure your listings remain competitive with our real-time AI insights.</p>
-      </div>
-    </div>
+    <SalePointSingle
+      src="/icons/eye_line.svg"
+      alt="mingcute:eye_line"
+      title="Boost Visibility"
+      desc="Increase your visibility and reach a wider audience with our AI based Optimization Service."
+    />
+    <SalePointSingle
+      src="/icons/book_6_line.svg"
+      alt="mingcute:book_6_line"
+      title="Turn Interest into Bookings"
+      desc="Turn potential guests into confirmed bookings with our smart and targeted AI algorithm based techniques."
+    />
+    <SalePointSingle
+      src="/icons/chart_line_line.svg"
+      alt="mingcute:chart_line_line"
+      title="Stay ahead of market trends"
+      desc="Stay ahead of market trends and ensure your listings remain competitive with our real-time AI insights."
+    />
   </div>
 </div>
