84967a77f19978e894353fb3579b285df9dde0c5
April Hall <ari@arithefirst.com>
2025-01-19 18:14:37 -0500

    fix: Adjust mobile sizing for audit component

--- src/lib/components/GetAudit.svelte
+++ src/lib/components/GetAudit.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="w-full text-center my-8">
-    <h1 class="text-2xl md:text-3xl">Optimize your listing <span class="text-primary font-bold">For Free</span> today</h1>
-    <form method="POST" action="?/scrape" class="flex gap-1 mx-auto w-2/3" use:enhance>
-        <input name="bnburl" type="text" placeholder="Your Airbnb URL here" class="input input-bordered w-3/4" />
+    <h1 class="text-2xl md:text-3xl">Optimize your listing <span class="text-primary font-bold">For Free</span></h1>
+    <form method="POST" action="?/scrape" class="flex md:flex-row flex-col gap-1 mx-auto w-2/3" use:enhance>
+        <input name="bnburl" type="text" placeholder="Your Airbnb URL here" class="input input-bordered md:w-3/4 w-full" />
         <button class="btn btn-primary">Optimize now</button>
     </form>
 </div>
