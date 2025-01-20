23c0b687b5f7e692bfe1437fd4bc9d32f9779ed3
April Hall <ari@arithefirst.com>
2025-01-20 15:22:24 -0500

    fix: Add url validation to audit form

--- src/lib/components/GetAudit.svelte
+++ src/lib/components/GetAudit.svelte
@@ -5,7 +5,7 @@
 <div class="my-8 w-full text-center">
   <h1 class="text-2xl md:text-3xl">Optimize your listing <span class="font-bold text-primary">For Free</span></h1>
   <form method="POST" action="?/scrape" class="mx-auto flex w-2/3 flex-col gap-1 md:flex-row" use:enhance>
-    <input name="bnburl" type="text" placeholder="Your Airbnb URL here" class="input input-bordered w-full md:w-3/4" />
+    <input name="bnburl" type="url" placeholder="Your Airbnb URL here" class="input input-bordered w-full md:w-3/4" />
     <button class="btn btn-primary">Optimize now</button>
   </form>
 </div>
