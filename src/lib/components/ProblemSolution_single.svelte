f7d54bfae4a1ccda91fdb29fddaae512264941db
April Hall <ari@arithefirst.com>
2025-01-23 13:30:38 -0500

    feat: Anims on problem solution cards

--- src/lib/components/ProblemSolution_single.svelte
+++ src/lib/components/ProblemSolution_single.svelte
@@ -7,7 +7,7 @@
   const { prob, sol }: props = $props();
 </script>
 
-<div class="card rounded-lg bg-base-200">
+<div class="intersect-once card rounded-lg bg-base-200 opacity-0 intersect:motion-preset-slide-right intersect:opacity-100">
   <div class="card-body text-center">
     <p class="text-xl font-semibold italic">"{prob}"</p>
     <p>{sol}</p>
