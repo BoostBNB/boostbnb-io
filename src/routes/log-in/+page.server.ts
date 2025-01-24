7c7c7296c6c5984ac171b9c2e4b93797dc5ecaef
April Hall <ari@arithefirst.com>
2025-01-23 23:07:10 -0500

    style: Prettier format

--- src/routes/log-in/+page.server.ts
+++ src/routes/log-in/+page.server.ts
@@ -1,52 +1,53 @@
 // src/routes/+page.server.ts
-import { fail, redirect } from '@sveltejs/kit'
-import type { Actions, PageServerLoad } from './$types'
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url, locals: { safeGetSession } }) => {
-  const { session } = await safeGetSession()
+  const { session } = await safeGetSession();
 
   // if the user is already logged in return them to the home page
   if (session) {
-    redirect(303, '/')
+    redirect(303, '/');
   }
 
-  return { url: url.origin }
-}
+  return { url: url.origin };
+};
 
 export const actions: Actions = {
   default: async (event) => {
     const {
       request,
       locals: { supabase },
-    } = event
-    const formData = await request.formData()
-    const email = formData.get('email') as string
+    } = event;
+    const formData = await request.formData();
+    const email = formData.get('email') as string;
     // eslint-disable-next-line no-useless-escape
-    const validEmail = /^[\w-\.+]+@([\w-]+\.)+[\w-]{2,8}$/.test(email)
+    const validEmail = /^[\w-\.+]+@([\w-]+\.)+[\w-]{2,8}$/.test(email);
 
     if (!validEmail) {
-      return fail(400, { errors: { email: 'Please enter a valid email address' }, email })
+      return fail(400, { errors: { email: 'Please enter a valid email address' }, email });
     }
 
-    const { error } = await supabase.auth.signInWithOtp({ 
+    const { error } = await supabase.auth.signInWithOtp({
       email,
       options: {
-      // set this to false if you do not want the user to be automatically signed up
-      shouldCreateUser: true,
-      emailRedirectTo: 'http://localhost:5173',
-    },})
+        // set this to false if you do not want the user to be automatically signed up
+        shouldCreateUser: true,
+        emailRedirectTo: 'http://localhost:5173',
+      },
+    });
 
     if (error) {
       return fail(400, {
         success: false,
         email,
         message: `There was an issue, Please contact support.`,
-      })
+      });
     }
 
     return {
       success: true,
       message: 'Please check your email for a magic link to log into the website.',
-    }
+    };
   },
-}
+};
