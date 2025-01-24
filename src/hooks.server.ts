c2313afd1c19737432a8ea1452dc6bc2bdc55b4b
April Hall <ari@arithefirst.com>
2025-01-23 23:07:10 -0500

    style: Prettier format

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
-import { createServerClient } from '@supabase/ssr'
-import type { Handle } from '@sveltejs/kit'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { createServerClient } from '@supabase/ssr';
+import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
   event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
@@ -13,11 +13,11 @@ export const handle: Handle = async ({ event, resolve }) => {
        */
       setAll: (cookiesToSet) => {
         cookiesToSet.forEach(({ name, value, options }) => {
-          event.cookies.set(name, value, { ...options, path: '/' })
-        })
+          event.cookies.set(name, value, { ...options, path: '/' });
+        });
       },
     },
-  })
+  });
 
   /**
    * Unlike `supabase.auth.getSession()`, which returns the session _without_
@@ -27,26 +27,26 @@ export const handle: Handle = async ({ event, resolve }) => {
   event.locals.safeGetSession = async () => {
     const {
       data: { session },
-    } = await event.locals.supabase.auth.getSession()
+    } = await event.locals.supabase.auth.getSession();
     if (!session) {
-      return { session: null, user: null }
+      return { session: null, user: null };
     }
 
     const {
       data: { user },
       error,
-    } = await event.locals.supabase.auth.getUser()
+    } = await event.locals.supabase.auth.getUser();
     if (error) {
       // JWT validation has failed
-      return { session: null, user: null }
+      return { session: null, user: null };
     }
 
-    return { session, user }
-  }
+    return { session, user };
+  };
 
   return resolve(event, {
     filterSerializedResponseHeaders(name) {
-      return name === 'content-range' || name === 'x-supabase-api-version'
+      return name === 'content-range' || name === 'x-supabase-api-version';
     },
-  })
-}
+  });
+};
