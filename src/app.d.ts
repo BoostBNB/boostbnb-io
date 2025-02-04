815754f78f1d79483f591d1bbfdd8393e858da47
April Hall <ari@arithefirst.com>
2025-02-04 13:09:52 -0500

    fix: Update Supabase type declarations

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,16 +1,20 @@
-import { SupabaseClient, Session } from '@supabase/supabase-js';
+import type { Session, SupabaseClient, User } from '@supabase/supabase-js'
 
 declare global {
   namespace App {
+    // interface Error {}
     interface Locals {
-      supabase: SupabaseClient;
-      safeGetSession(): Promise<{ session: Session | null; user: User | null }>;
+      supabase: SupabaseClient
+      safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
+      session: Session | null
+      user: User | null
     }
     interface PageData {
-      session: Session | null;
-      user: User | null;
+      session: Session | null
     }
-    // interface Error {}
+    // interface PageState {}
     // interface Platform {}
   }
 }
+
+export {}
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,4 +2,8 @@
   import Dashboard from '$lib/components/dashboard/index.svelte';
 </script>
 
+<svelte:head>
+  <title>BoostBNB | Dashboard</title>
+</svelte:head>
+
 <Dashboard />
