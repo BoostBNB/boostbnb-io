c8ddffea2661add2bf4f0df1078c60ce44bd0e43
April Hall <ari@arithefirst.com>
2025-02-05 12:10:19 -0500

    feat: Implement schema validation for audit

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,26 +1,34 @@
 import type { Actions } from './$types';
+import { superValidate, message } from 'sveltekit-superforms';
+import { fail } from '@sveltejs/kit';
+import { zod } from 'sveltekit-superforms/adapters';
 import auditListing from '$lib/audit';
+import { z } from 'zod';
+
+const schema = z.object({
+  url: z.string().url(),
+  email: z.string().email(),
+});
+
+export const load = async () => {
+  const form = await superValidate(zod(schema));
+  return { form };
+};
 
 export const actions: Actions = {
   audit: async ({ request }) => {
-    console.log('Auditing');
-    const data = await request.formData();
-    const url = data.get('bnburl');
-    const email = data.get('email');
+    const form = await superValidate(request, zod(schema));
+    const url = form.data.url;
+    const email = form.data.email;
 
-    // Validate the URL
-    if (!url || typeof url !== 'string') {
-      return { success: false, error: 'Invalid URL format' };
-    }
-
-    if (!email || typeof email !== 'string') {
-      return { success: false, error: 'Enter a valid email' };
+    if (!form.valid) {
+      return fail(400, { form });
     }
 
     // Call the scraper function and log the result
     const result = await auditListing(url, email);
     console.log('Audit result:', result);
 
-    return { success: true, result };
+    return message(form, 'Form posted successfully!');
   },
 };
