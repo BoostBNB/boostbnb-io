404d33b2f3a54ea6fcafa305ace3428aeeed5101
April Hall <ari@arithefirst.com>
2025-02-05 12:35:52 -0500

    feat: Implement schema validation for login

--- src/routes/log-in/+page.svelte
+++ src/routes/log-in/+page.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
-  import { enhance } from '$app/forms';
-  import type { ActionData, SubmitFunction } from './$types';
+  import { superForm } from 'sveltekit-superforms';
+  import SuperDebug from 'sveltekit-superforms';
 
-  interface props {
-    form: ActionData;
-  }
-
-  const { form }: props = $props();
-  let loading = $state(false);
-
-  const handleSubmit: SubmitFunction = () => {
-    loading = true;
-    return async ({ update }) => {
-      update();
-      loading = false;
-    };
-  };
+  let { data } = $props();
+  const { form, errors, constraints, message, enhance } = superForm(data.form);
 </script>
 
 <svelte:head>
@@ -25,25 +13,34 @@
 <div class="h-screen w-screen bg-gradient-to-br from-white to-blue-200">
   <div class="size-full bg-nav backdrop-blur-md backdrop-filter">
     <form
-      class="absolute left-1/2 top-1/2 flex w-3/4 -translate-x-1/2 -translate-y-1/2 flex-col rounded-md bg-base-200 p-6 text-center md:w-1/3"
+      class="absolute left-1/2 top-1/2 flex w-3/4 -translate-x-1/2 -translate-y-1/2 flex-col gap-1 rounded-md bg-base-200 p-6 text-center md:w-1/3"
       method="POST"
-      use:enhance={handleSubmit}
+      use:enhance
     >
       <h1 class="mb-2 text-2xl font-bold">Login</h1>
-      <input type="email" id="email" name="email" class="input input-bordered mb-2" placeholder="janedoe@example.com" value={form?.email ?? ''} />
-      {#if form?.errors?.email}
-        <span class="error flex items-center text-sm">
-          {form?.errors?.email}
-        </span>
-      {/if}
-      <input type="password" id="password" name="password" class="input input-bordered mb-2" placeholder="Password" />
-      <button class="btn btn-primary">{loading ? 'Loading' : 'Log In'}</button>
-      {#if form?.message !== undefined}
-        <div class="success {form?.success ? '' : 'fail'}">
-          {form?.message}
-        </div>
-      {/if}
+      <input
+        name="email"
+        bind:value={$form.email}
+        aria-invalid={$errors.email ? 'true' : undefined}
+        {...$constraints.email}
+        class="input input-bordered"
+        placeholder="janedoe@example.com"
+      />
+      {#if $errors.email}<span class="text-error">{$errors.email}</span>{/if}
+      <input
+        name="password"
+        type="password"
+        bind:value={$form.password}
+        aria-invalid={$errors.password ? 'true' : undefined}
+        {...$constraints.password}
+        class="input input-bordered"
+        placeholder="Password"
+      />
+      {#if $errors.password}<span class="text-error">{$errors.password}</span>{/if}
+      {#if $message}<h3 class="text-success">{$message}</h3>{/if}
+      <button class="btn btn-primary" type="submit">Log In</button>
       <p class="mt-2">Don't have an account? <a href="/sign-up" class="link">Sign Up</a>.</p>
+      <SuperDebug data={$form} />
     </form>
   </div>
 </div>
