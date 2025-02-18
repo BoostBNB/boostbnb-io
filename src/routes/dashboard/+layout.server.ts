ff65abd9dad3f0ef2067e26f0b0a38bfdebad0ec
SpaceAgeX <104997246+SpaceAgeX@users.noreply.github.com>
2025-02-17 22:05:37 -0500

    fix: load function bug

--- src/routes/dashboard/+layout.server.ts
+++ src/routes/dashboard/+layout.server.ts
@@ -3,15 +3,19 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async ({ locals }) => {
   const { user } = locals.session || {};
   if (!user) {
-    return { listings: [] }; // Return an empty array if user is not authenticated
+    return { listings: [] }; 
   }
 
-  const { data, error } = await locals.supabase.from('listings').select('url, data').eq('user_id', user.id).single();
+  
+  const { data, error } = await locals.supabase
+    .from('listings')
+    .select('url, data') 
+    .eq('user_id', user.id);
 
   if (error) {
     console.error('Error fetching listings:', error.message);
     return { listings: [] };
   }
 
-  return { listings: data || [] };
+  return { listings: data || [] }; 
 };
