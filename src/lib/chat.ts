e235e2f4ec999a4973b7f934068ab59601761ce3
Justin Ebanks <justinebanks580@gmail.com>
2025-03-04 21:20:51 -0500

    feat: Adds Cohost AI

--- src/lib/chat.ts
+++ src/lib/chat.ts
@@ -2,8 +2,9 @@ import OpenAI from 'openai';
 
 const openai = new OpenAI({
   apiKey:
-    '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',
-});
+    //'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',
+    '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'
+  });
 
 export async function askChatGPT(prompt: string): Promise<string> {
   try {
--- src/routes/dashboard/chat/+page.svelte
+++ src/routes/dashboard/chat/+page.svelte
@@ -1,5 +1,14 @@
+<script>
+  import { enhance } from '$app/forms';
+</script>
+
 <svelte:head>
   <title>BoostBNB | Cohost AI</title>
 </svelte:head>
 
 Cohost AI will be shown here
+
+<form method="POST" action="?/chatResponse" class="mx-auto flex w-2/3 flex-col items-center gap-1" use:enhance>
+  <input name="prompt" type="text" placeholder="Type in Prompt" class="input input-bordered w-full md:w-3/4" />
+  <button class="btn btn-primary w-full md:w-1/4">Get Response</button>
+</form>
